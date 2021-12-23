96b8ae1b79cebca5d12a980eed5b1247efe23536
shakedx <kolesnikov_danil123@mail.ru>
2021-12-24 01:52:24 +0200

    Добавьте файлы проекта.

---
+++ functions6.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace functions6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите a:");
+            int a = int.Parse(Console.ReadLine());
+            
+            {
+                if (a >= 0) Console.WriteLine(a);
+                else Console.WriteLine(-a);
+
+            }
+            static double Func(int x, int y)
+            {
+                Console.WriteLine("Введите a:");
+                int a = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
